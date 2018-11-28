9440fc878590d1a78661210bedd486186f67637e
Yoseph <Yoseph@DESKTOP-4N6CJL1>
2018-11-28 17:38:23 +0700

    BTSSERVICES

---
+++ ServicesBTS/src/java/services/daftarLokasiResource.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+/**
+ * REST Web Service
+ *
+ * @author Yoseph
+ */
+@Path("daftarLokasi")
+public class daftarLokasiResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of daftarLokasiResource
+     */
+    public daftarLokasiResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of services.daftarLokasiResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of daftarLokasiResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
