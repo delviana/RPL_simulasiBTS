da9683a8379124210db900432c1887ca8e43affa
Gandhi Nugroho <Gandhi Nugroho@GandhiNugroho>
2018-12-06 07:26:03 +0700

    Helper User & User RSS

--- serviceSimulasi/src/java/helper/userHelper.java
+++ serviceSimulasi/src/java/helper/userHelper.java
@@ -5,10 +5,84 @@
  */
 package helper;
 
+import java.util.Collections;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.NewHibernateUtil;
+
 /**
  *
  * @author Acer
  */
 public class userHelper {
-    
+    public List<User> getAllUser() {
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        List<User> hasil = null;
+        Query q = session.createQuery("from User u");
+        hasil = q.list();
+        tx.commit();
+        session.close();
+        return hasil;
+    }
+
+    public User getUser(String username, String password) {
+        List<User> list = this.getAllUser();
+        User user = new User(username, password);
+        Collections.sort(list);
+        int index = -1;
+        if (list.size() > -1) {
+            for (int i = 0; i < list.size(); i++) {
+                if (username.equals(list.get(i).getUsername())) {
+                    index = i;
+                }
+            }
+        } else {
+            index = -1;
+        }
+
+        if (index < 0) {
+            System.out.println("User tidak ada");
+            return null;
+        } else {
+            // user ada
+            User result = list.get(index);
+            System.out.println("Pencarian = " + result.getUsername() + "," + result.getPassword());
+            if (password.equals(result.getPassword())) {
+                System.out.println("Username: " + user.getUsername() + "\nPassword: " + user.getPassword());
+                return user;
+            } else {
+                System.out.println("pass salah");
+                // pass salah
+                return null;
+            }
+        }
+    }
+
+    public static String toJson() {
+        UserHelper helper = new UserHelper();
+        List<User> list = helper.getAllUser();
+        String result = "[";
+        for (int i = 0; i < list.size(); i++) {
+            if (i < (list.size() - 1)) {
+                result += list.get(i).toJson() + ", \n";
+            } else {
+                result += list.get(i).toJson();
+            }
+        }
+        result += "]";
+        return result;
+    }
+    public void addNewUser(
+          String username,String password
+    ) {
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        User user = new User(password, username);
+        session.saveOrUpdate(user);
+        tx.commit();
+        session.close();
+    }
 }
--- serviceSimulasi/src/java/service/userResource.java
+++ serviceSimulasi/src/java/service/userResource.java
@@ -5,14 +5,19 @@
  */
 package service;
 
+import com.google.gson.Gson;
+import helper.userHelper;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -36,18 +41,40 @@ public class userResource {
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-    return "Hello World";   
     
-    }
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJson() {
+        userHelper test = new userHelper();
+        List<User> list = test.getAllUser();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return Response
+                .status(200)
+                .entity(json)
+                .build();
+    } 
+    
+    
 
     /**
      * PUT method for updating or creating an instance of userResource
      * @param content representation for the resource
      */
-    @PUT
+//    @PUT
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public void putJson(String content) {
+//    }
+        @POST
+    @Path("login")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public Response login(String data) {
+        Gson gson = new Gson();
+        User user = gson.fromJson(data, User.class);
+        userHelper helper = new userHelper();
+        helper.getUser(user.getUsername(), user.getPassword());
+        return Response
+                .status(200)
+                .entity(helper.getUser(user.getUsername(), user.getPassword()))
+                .build();
     }
 }
