4ced1ae75ac50f310b6cee353ed9c003fb6b5aad
Gandhi Nugroho <Gandhi Nugroho@GandhiNugroho>
2018-12-09 12:40:40 +0700

    Helper JaringanEks & RSS

--- serviceSimulasi/src/java/helper/jaringanEksHelper.java
+++ serviceSimulasi/src/java/helper/jaringanEksHelper.java
@@ -5,10 +5,54 @@
  */
 package helper;
 
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pojos.JaringanEks;
+import util.NewHibernateUtil;
+
 /**
  *
  * @author Acer
  */
 public class jaringanEksHelper {
-    
+   
+
+     public jaringanEksHelper() {
+
+    }
+     
+
+  public void addNewDataJaringanEksisting(
+     String CoverageArea,
+     String namaBTS,
+     String jumlahBTS,
+     String kekuatanSinyal
+         
+          
+  ){
+         Session session = NewHibernateUtil.getSessionFactory().openSession();
+           Transaction tx = session.beginTransaction();
+           JaringanEks datajaringanEks=new JaringanEks(CoverageArea, namaBTS, jumlahBTS, kekuatanSinyal);
+           session.saveOrUpdate(datajaringanEks);
+           tx.commit();
+           session.close();
+  }
+  public List<JaringanEks> bacaSemuaDataJaringanEks() {
+        List<JaringanEks> list = null;
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("from JaringanEksisting p");
+        list = q.list();
+        tx.commit();
+        session.close();
+        return list;
+
+        
+    }
+  public void updateDataDarah(){
+      
+  } 
 }
--- serviceSimulasi/src/java/service/JaringanEksResource.java
+++ serviceSimulasi/src/java/service/JaringanEksResource.java
@@ -5,14 +5,19 @@
  */
 package service;
 
+import com.google.gson.Gson;
+import helper.jaringanEksHelper;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pojos.JaringanEks;
 
 /**
  * REST Web Service
@@ -36,10 +41,25 @@ public class JaringanEksResource {
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
+     @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response getJson() {
         //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        jaringanEksHelper p = new jaringanEksHelper();
+        Gson gson = new Gson();
+        return Response.status(Response.Status.OK)
+                .entity(gson.toJson(p.bacaSemuaDataJaringanEks()))
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods",
+                        "GET,POST,HEAD,OPTIONS,PUT")
+                .header("Access-Control-Allow-Headers",
+                        "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers")
+                .header("Access-Exposed-Headers",
+                        "Access-Control-Allow-Origin,Access-Control-Allow-Credentials")
+                .header("Access-Support-Credentials",
+                        "true")
+                .header("Access-Control-Max-Age", "20")
+                .header("Access-Preflight-Maxage", "20")
+                .build();
     }
 
     /**
@@ -49,5 +69,19 @@ public class JaringanEksResource {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
+    }
+    @POST
+      @Path("addJaringanEks")
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response AddNewJaringanEks(String data) {
+        Gson gson = new Gson();
+        JaringanEks dataJaringanEks = gson.fromJson(data, JaringanEks.class);
+        jaringanEksHelper helper = new jaringanEksHelper();
+        helper.addNewDataJaringanEksisting(
+                dataJaringanEks.getIdjaringan(),
+                dataJaringanEks.getLat(),
+                dataJaringanEks.getLong_(),
+                dataJaringanEks.getPowerSinyal());
+        return Response.status(200).entity(dataJaringanEks).build();
     }
 }
