cb1de57eea4597737537abe463e626f4d5a1d0ac
Gandhi Nugroho <Gandhi Nugroho@GandhiNugroho>
2018-12-18 06:53:27 +0700

    Revisi Lokasi Kampung Rss Final

--- serviceSimulasi/src/java/service/LokasiKampungResource.java
+++ serviceSimulasi/src/java/service/LokasiKampungResource.java
@@ -7,6 +7,7 @@ package service;
 
 import com.google.gson.Gson;
 
+
 import helper.lokasiKampungHelper;
 import java.util.List;
 import javax.ws.rs.core.Context;
@@ -14,10 +15,12 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 
 import pojos.LokasiDesa;
 
@@ -64,5 +67,19 @@ public class LokasiKampungResource {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
+    }
+     @POST
+      @Path("addLokasiDesa")
+    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+    public Response AddNewLokasiKampung(String data) {
+        Gson gson = new Gson();
+        LokasiDesa dataLokasiDesa = gson.fromJson(data, LokasiDesa.class);
+        lokasiKampungHelper helper = new lokasiKampungHelper();
+        helper.addNewLokasiKampung(
+                dataLokasiDesa.getIdDesa(),
+                dataLokasiDesa.getLat(),
+                dataLokasiDesa.getLog(),
+                dataLokasiDesa.getJumPenduduk());
+        return Response.status(200).entity(dataLokasiDesa).build();
     }
 }
